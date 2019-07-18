cdf2f82d64f8c8f5b57e95b312432249518ad4bb
Dano <danielaherniv@gmail.com>
2019-07-18 11:32:56 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,22 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    var yellowGem = 0;
+
+
+    $("#yellowGem").on("click", function () {
+
+        yellowGem++;
+
+        console.log(yellowGem);
+
+        document.getElementById("yellowGem").innerHTML.yellowGem;
+
+
+    });
+
+});
 
 
 
 
 
-
-
-
-}
